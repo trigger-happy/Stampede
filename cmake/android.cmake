27e3fac0432fc2345606f3ba7740fb0de6883000
James Choa <triggerhappyelite@gmail.com>
2013-05-14 22:47:13 +0800

    Compilation on android

--- cmake/android.cmake
+++ cmake/android.cmake
@@ -4,5 +4,47 @@ set(
 	${ORX_DEV_DIR}/dev-android
 )
 
+set(
+	ORX_EXTERN_DIR
+	${ORX_DEV_DIR}/extern
+)
+
 add_definitions(-D__orxANDROID__)
-link_directories(${ORX_PLATFORM_DIR}/lib/static/android/armeabi-v7a)
+link_directories(
+	${ORX_PLATFORM_DIR}/lib/static/android/armeabi-v7a
+	${ORX_EXTERN_DIR}/Box2D_2.1.3/lib/android/armeabi-v7a
+	${ORX_EXTERN_DIR}/openal-soft/lib/android/armeabi-v7a
+	${ORX_EXTERN_DIR}/SOIL/lib/android/armeabi-v7a
+	${ORX_EXTERN_DIR}/Tremor/lib/android/armeabi-v7a
+	
+	#TODO: remove the hard coded paths
+	/home/trigger/src/Boost-for-Android/build/lib
+	/opt/android-ndk/platforms/android-14/arch-arm/usr/lib
+)
+
+set(
+	ORX_EXTERN_LIBS
+	
+	Tremor
+	SOIL
+	openal-soft
+	Box2D
+	log
+	GLESv2
+	OpenSLES
+	android
+)
+
+# force the boost libs
+set(
+	Boost_LIBRARIES
+	
+	boost_date_time
+	boost_filesystem
+	boost_iostreams
+	boost_program_options
+	boost_regex
+	boost_signals
+	boost_system
+	boost_thread
+)
